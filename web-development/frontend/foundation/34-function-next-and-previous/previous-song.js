61be99ab793ba2b4b726a1a7108dfb920a747f9e
Joe Lipper <lipperjj@gmail.com>
2015-08-24 20:44:52 -0700

    updated nextSong() and previousSong() to remove methods that aren't defined until the assignment

--- web-development/frontend/foundation/34-function-next-and-previous/previous-song.js
+++ web-development/frontend/foundation/34-function-next-and-previous/previous-song.js
@@ -1,18 +1,28 @@
 var previousSong = function() {
     
+    // Note the difference between this implementation and the one in
+    // nextSong()
     var getLastSongNumber = function(index) {
         return index == (currentAlbum.songs.length - 1) ? 1 : index + 2;
     };
-  
+    
     var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+    // Note that we're _decrementing_ the index here
     currentSongIndex--;
     
     if (currentSongIndex < 0) {
         currentSongIndex = currentAlbum.songs.length - 1;
     }
     
-    setSong(currentSongIndex + 1);
-    updatePlayerBarSong();
+    // Set a new current song
+    currentlyPlayingSongNumber = currentSongIndex + 1;
+    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+    // Update the Player Bar information
+    $('.currently-playing .song-name').text(currentSongFromAlbum.name);
+    $('.currently-playing .artist-name').text(currentAlbum.name);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.name + " - " + currentAlbum.name);
+    $('.left-controls .play-pause').html(playerBarPauseButton);
     
     var lastSongNumber = getLastSongNumber(currentSongIndex);
     var $previousSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
