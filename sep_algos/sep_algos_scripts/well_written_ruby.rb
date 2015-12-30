2a086fda2c6cd7fd36fd7ec2f559ad5b18bfab60
robmadden <rob.madden@bloc.io>
2015-12-29 16:27:07 -0800

    Add poorly and well written ruby for Improving Complexity CP

---
+++ sep_algos/sep_algos_scripts/well_written_ruby.rb
@@ -0,0 +1,29 @@
+def merge_sort(collection)
+  if collection.length <= 1
+    collection
+  else
+    mid = (collection.length / 2).floor
+    left = merge_sort(collection[0..mid - 1])
+    right = merge_sort(collection[mid..collection.length])
+    merge(left, right)
+  end
+end
+
+def merge(left, right)
+  if left.empty?
+    right
+  elsif right.empty?
+    left
+  elsif left.first < right.first
+    [left.first] + merge(left[1..left.length], right)
+  else
+    [right.first] + merge(left, right[1..right.length])
+  end
+end
+ 
+def well_written_ruby(*arrays)
+  merge_sort(arrays.flatten)
+end
+
+puts well_written_ruby([4, 3, 2], [1, 5, 6], [8, 7 ,9])
+
