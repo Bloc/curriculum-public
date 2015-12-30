e21f36ec4c6cc131c7c962a7375c1408362db093
robmadden <rob.madden@bloc.io>
2015-12-30 13:41:14 -0800

    Add a better find largest script

---
+++ sep_algos/sep_algos_scripts/find_largest_better.rb
@@ -0,0 +1,31 @@
+def find_largest_better(collection)
+  largest = collection[0]
+  collection_length = collection.length
+
+  (0..collection_length/2).each do |i|
+    left_element = collection[i]
+    right_element = collection[collection_length - i - 1]
+
+    l = right_element > left_element ? right_element : left_element
+
+    if l > largest
+      largest = l
+    end
+  end
+
+  largest
+end
+
+def create_collection(size)
+  collection = Array.new(size) { |i| rand(1000) } 
+  collection
+end
+
+def flb(collection)
+  puts find_largest_better(collection)
+end
+
+flb([1, 4, 5, 2, 3, 10])
+flb([11, 4, 5, 2, 3, 10, 1, 4, 5, 2, 3, 10])
+flb([11, 4, 5, 2, 3, 10, 21, 4, 5, 2, 3, 10])
+flb(create_collection(100))
