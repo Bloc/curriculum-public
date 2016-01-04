e3eca5d99b41d50b131b249ad4fc585f87a1e384
robmadden <rob.madden@bloc.io>
2016-01-04 15:26:10 -0800

    Update to old, bad code

--- sep_algos/sep_algos_scripts/poorly_written_ruby.rb
+++ sep_algos/sep_algos_scripts/poorly_written_ruby.rb
@@ -1,30 +1,31 @@
- def well_written_ruby(*arrays)
-   combined_array = []
-   arrays.each do |array|
-     array.each do |value|
-       combined_array << value
-     end
-   end
+# This method takes n arrays as input and combine them in sorted ascending order
+def poorly_written_ruby(*arrays)
+  combined_array = []
+  arrays.each do |array|
+    array.each do |value|
+      combined_array << value
+    end
+  end
 
-   sorted_array = [combined_array.delete_at(combined_array.length-1)]
+  sorted_array = [combined_array.delete_at(combined_array.length-1)]
 
-   for val in combined_array
-     i = 0
-     while i < sorted_array.length
-       if val <= sorted_array[i]
-         sorted_array.insert(i, val)
-         break
-       elsif i == sorted_array.length - 1
-         sorted_array.insert(i, val)
-         break
-       end
-       i+=1
-     end
-   end
+  for val in combined_array
+    i = 0
+    while i < sorted_array.length
+      if val <= sorted_array[i]
+        sorted_array.insert(i, val)
+        break
+      elsif i == sorted_array.length - 1
+        sorted_array.insert(i, val)
+        break
+      end
+      i+=1
+    end
+  end
 
-   # Return the sorted array
-   sorted_array
- end
+  # Return the sorted array
+  sorted_array
+end
 
 puts poorly_written_ruby([4, 3, 2], [1, 5, 6], [8, 7 ,9])
 
