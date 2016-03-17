ba03c8321f253e2f771eee31681e8b358628fbf6
Colleen <colleen@bloc.io>
2016-03-17 09:11:50 -0700

    Change name to title

--- web-development/frontend/foundation/32-jquery-next-and-previous-buttons/next-song.js
+++ web-development/frontend/foundation/32-jquery-next-and-previous-buttons/next-song.js
@@ -17,9 +17,9 @@ var nextSong = function() {
     currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
     // Update the Player Bar information
-    $('.currently-playing .song-name').text(currentSongFromAlbum.name);
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
     $('.currently-playing .artist-name').text(currentAlbum.artist);
-    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.name + " - " + currentAlbum.name);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.title);
     $('.main-controls .play-pause').html(playerBarPauseButton);
     
     var lastSongNumber = getLastSongNumber(currentSongIndex);
