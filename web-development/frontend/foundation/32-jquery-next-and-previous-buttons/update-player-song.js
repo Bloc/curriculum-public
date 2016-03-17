9df33825ad52803321c4376a3d8901b902e1d09d
Colleen <colleen@bloc.io>
2016-03-17 08:44:13 -0700

    Change from name to title

--- web-development/frontend/foundation/32-jquery-next-and-previous-buttons/update-player-song.js
+++ web-development/frontend/foundation/32-jquery-next-and-previous-buttons/update-player-song.js
@@ -5,9 +5,9 @@ var setCurrentAlbum = function(album) {
 
 var updatePlayerBarSong = function() {
 
-    $('.currently-playing .song-name').text(currentSongFromAlbum.name);
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
     $('.currently-playing .artist-name').text(currentAlbum.artist);
-    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.name + " - " + currentAlbum.artist);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
 
 };
 ...
