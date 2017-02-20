ed997a6d90fcf2afcf8e063b33525dc96a275719
Kevin McGillivray <kevin.mcgillivray@me.com>
2017-02-20 11:50:37 -0600

    Update next and previous functions for frontend
    These functions should be using `updatePlayerBarSong`, a function
    written earlier in the frontend checkpoint, "jQuery: Next and
    Previous Buttons". The code was out of sync with that function,
    and should just call it anyway. See https://phabricator.bloc.io/T5259

--- web-development/frontend/foundation/32-jquery-next-and-previous-buttons/next-song.js
+++ web-development/frontend/foundation/32-jquery-next-and-previous-buttons/next-song.js
@@ -1,32 +1,29 @@
 var nextSong = function() {
-    
+
     var getLastSongNumber = function(index) {
         return index == 0 ? currentAlbum.songs.length : index;
     };
-    
+
     var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
     // Note that we're _incrementing_ the song here
     currentSongIndex++;
-    
+
     if (currentSongIndex >= currentAlbum.songs.length) {
         currentSongIndex = 0;
     }
-    
+
     // Set a new current song
     currentlyPlayingSongNumber = currentSongIndex + 1;
     currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
 
     // Update the Player Bar information
-    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
-    $('.currently-playing .artist-name').text(currentAlbum.artist);
-    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.title);
-    $('.main-controls .play-pause').html(playerBarPauseButton);
-    
+    updatePlayerBarSong();
+
     var lastSongNumber = getLastSongNumber(currentSongIndex);
     var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
     var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
-    
+
     $nextSongNumberCell.html(pauseButtonTemplate);
     $lastSongNumberCell.html(lastSongNumber);
-    
+
 };
