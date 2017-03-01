d550115d93eb5afc8c96b3a90663e04cb1896528
Tessa Siegel <Tessa Siegel>
2017-03-01 12:12:29 -0800

    Changes while loop to use strict equality in both clauses

--- web-development/frontend/foundation/27-dom-scripting-play-pause-part-2/find-parent-by-class-name.js
+++ web-development/frontend/foundation/27-dom-scripting-play-pause-part-2/find-parent-by-class-name.js
@@ -1,7 +1,7 @@
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
-        while (currentParent.className != targetClass && currentParent.className !== null) {
+        while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
         return currentParent;
